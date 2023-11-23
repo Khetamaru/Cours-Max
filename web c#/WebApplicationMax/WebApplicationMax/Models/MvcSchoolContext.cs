2b337c4966665888208fb1b5c770080d4e9bfd30
Khetamaru <lasserre@intechinfo.fr>
2023-11-23 21:45:41 +0100

    Cours pret

---
+++ web c#/WebApplicationMax/WebApplicationMax/Models/MvcSchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationMax.Views.Home;
+
+namespace WebApplicationMax.Models
+{
+    public class MvcSchoolContext : DbContext
+    {
+        public MvcSchoolContext(DbContextOptions<MvcSchoolContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
